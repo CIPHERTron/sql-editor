d80ab4f57be1fde5c02b0ecbe53962628dbdb74e
CIPHERTron <pritish.samal918@gmail.com>
2023-07-26 00:38:51 +0530

    chore: add test route

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -1,14 +1,16 @@
 import React from 'react';
 
-import { HashRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 
 const Home = React.lazy(() => import('pages/Home'));
+const Playground = React.lazy(() => import('pages/Playground'));
 
 const Routes = () => {
 	return (
 		<Router>
 			<Switch>
 				<Route exact path="/" component={Home} />
+				<Route exact path="/test" component={Playground} />
 			</Switch>
 		</Router>
 	);
