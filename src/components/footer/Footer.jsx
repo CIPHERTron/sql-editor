912fbc7c0468b167b8412e80a1ca2c03ee7d2a97
CIPHERTron <pritish.samal918@gmail.com>
2023-07-26 15:27:50 +0530

    feat: add query execution time

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from 'react';
 
+import styled from '@emotion/styled';
+import { JoinFull } from '@mui/icons-material';
+import { IconButton, Typography } from '@mui/material';
+
+const Flex = styled.div`
+	display: flex;
+	flex-direction: row;
+	justify-content: space-around;
+	align-items: center;
+	height: 100px;
+	background-color: #1876d1;
+	color: #fff;
+	margin-top: 30%;
+`;
+
+const Branding = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: center;
+`;
+
 function Footer() {
 	return (
-		<div>
-			<h1>Footer</h1>
-		</div>
+		<Flex>
+			<Branding>
+				<IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 0 }}>
+					<JoinFull />
+				</IconButton>
+				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+					SQL Editor
+				</Typography>
+			</Branding>
+
+			<Typography>Designed and Developed with 💙 by CIPHERTron</Typography>
+		</Flex>
 	);
 }
 
